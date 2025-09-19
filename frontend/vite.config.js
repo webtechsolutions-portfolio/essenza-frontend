9f091df324b9cb97c2bb4c68dd95dbebdecaa53b
Zeazelu <zeazelu@gmail.com>
2025-09-19 13:48:11 +0200

    fix deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/essenza-aesthetic/", // <- nazwa repozytorium!
   server: {
     proxy: {
       "/api": "http://localhost:5000",
